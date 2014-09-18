89043508e7f2e9949484251f4328423c09a12c10
Chris Blackburn <christopher.blackburn@gmail.com>
2014-09-18 14:18:38 +0100

    Fixing clash between class and module name

--- app/models/timeline/event.rb
+++ app/models/timeline/event.rb
@@ -4,7 +4,7 @@ module Timeline
 
     attr_accessible :year, :title, :description, :image, :timeline_id
 
-    has_attached_file :image, styles: Timeline.config.image_styles
+    has_attached_file :image, styles: ::Timeline.config.image_styles
 
     default_scope order: '`year` ASC'
 
